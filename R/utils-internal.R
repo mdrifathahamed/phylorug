f2f00b648a8184e3e9062235b336b03854ee7f39
mdrifathahamed <rifathahamed777@gmail.com>
2026-06-18 14:07:35 +0300

    2026-06-18: add normalize_support internal utility

--- R/utils-internal.R
+++ R/utils-internal.R
@@ -21,3 +21,19 @@ detect_format <- function(path) {
   # Default - Newick
   "newick"
 }
+# normalize_support()
+# Rescale a vector of support values to the 0-1 range.
+# Bootstrap-style values (0-100) are divided by 100; values already on a
+# 0-1 scale are returned unchanged. Detection is per vector: if any value
+# exceeds 1, the whole vector is treated as 0-100. All-NA vectors pass
+# through untouched. Called at colour time, once per tree column.
+normalize_support <- function(x) {
+  if (all(is.na(x))) {
+    return(x)
+  }
+  if (any(x > 1, na.rm = TRUE)) {
+    x / 100
+  } else {
+    x
+  }
+}
